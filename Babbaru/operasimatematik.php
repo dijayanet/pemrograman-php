e204560eb4ebca50552fed03a062a5a00e1765ec
dijaya.net <ramdanahmaddijaya34.com>
2024-05-11 22:21:10 +0700

    bab baru

---
+++ Babbaru/operasimatematik.php
@@ -0,0 +1,16 @@
+<?php
+echo "OPERASI MATEMATIK";
+echo "<br> Tambah (+) :  4 + 2 = ";
+echo 4 + 2;
+
+echo "<br> Kurang (-) : 12 - 3 = ";
+echo 12 - 3;
+
+echo "<br> Kali (*) : 3*3 = ";
+echo 3*3;
+ 
+echo "<br> Bagi (/) : 12 / 4 = ";
+echo 12 / 4;
+
+echo "<br> Modulus (%) : 35 MOD 4 =  ";
+echo 15 % 4;
